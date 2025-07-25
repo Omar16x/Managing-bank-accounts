8dda3576b7dd2461aec3fc854adb71ecd0cd7531
Omar16x <omar_16x@hotmail.com>
2025-07-25 22:21:09 +0200

    chore: intial commit

---
+++ bankaccountmanager/src/main/java/com/bank/accounts/BankAccount.java
@@ -0,0 +1,62 @@
+package com.bank.accounts;
+
+/**
+ * Abstract class representing a bank account.
+ */
+public abstract class BankAccount {
+    protected final String accountNumber;
+    protected final String accountHolder;
+    protected double balance;
+
+    /**
+     * Constructor for BankAccount.
+     *
+     * @param accountNumber the account number
+     * @param accountHolder the name of the account holder
+     * @param balance       the initial balance of the account
+     */
+    protected BankAccount(String accountNumber, String accountHolder, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
+
+    /** Method to deposit an amount of money into the account.
+     * @param amount The amount to withdraw.
+     */
+    public abstract void deposit(double amount);
+    /** Method to withdraw an amount of money from the account.
+     * @param amount The amount to withdraw.
+     */
+    public abstract void withdraw(double amount);
+    /** Method to calculate interest.
+     */
+    public abstract void calculateInterest();
+
+    /**
+     * Method to get the current balance.
+     *
+     * @return the balance
+     */
+    public double getBalance() {
+        return balance;
+    }
+
+    /**
+     * Method to get the account number.
+     *
+     * @return the account number
+     */
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    /**
+     * Method to get the account holder.
+     *
+     * @return the account holder
+     */
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+}
---
+++ bankaccountmanager/src/main/java/com/bank/application/BankaccountmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.bank.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankaccountmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankaccountmanagerApplication.class, args);
+	}
+
+}
